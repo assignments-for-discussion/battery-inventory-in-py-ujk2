0cb1906d12f720541e6b2a9598c09b76685f7f2e
Ujwal <ujwalamin2001@gmail.com>
2022-10-31 17:40:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 
 def count_batteries_by_usage(cycles):
+  lowCount = 0
+  mediumCount = 0
+  highCount = 0
+ 
+  for i in cycles:
+    if i<=0:
+      continue
+    if i < 410:
+      lowCount = lowCount+1
+        elif i >= 410 and i <= 949:
+          mediumCount = mediumCount+1
+        elif i >= 950:
+          highCount = highCount+1
   return {
-    "lowCount": 0,
-    "mediumCount": 0,
-    "highCount": 0
+    "lowCount": lowCount,
+    "mediumCount": mediumCount,
+    "highCount": highCount
   }
 
 
